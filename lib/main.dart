bad8f985d87e1a1bd1552c7b6fd1a9f38b513a5e
impuranjayp <puranjaypotnis1@gmail.com>
2021-08-18 20:58:24 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,49 +12,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                //height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                //child: Text('Container 2'),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Profile_Pic.png'),
+              ),
+              Text(
+                'Puranjay Nayan Potnis',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(
-                width: 55.0,
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                    //child: Text('Container 2'),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
                   ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                    //child: Text('Container 2'),
+                  title: Text(
+                    '+91 810 823 0070',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
                   ),
-                ],
+                ),
               ),
-              SizedBox(
-                width: 55.0,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'puranjaypotnis1@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                //height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                //child: Text('Container 2'),
-              )
             ],
-          ),
+          )
           ),
         ),
       );
   }
 }
-
